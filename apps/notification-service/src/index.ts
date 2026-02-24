fd6a2f71156a6bb2b28d6b5adcc0f6dad06c7267
YourGitHubUsername <ramuk13476@gmail.com>
2026-02-24 22:18:32 +0530

    add the depen and endpoint

--- apps/notification-service/src/index.ts
+++ apps/notification-service/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+
+import Hono from "hono";
+
+//@ts-ignore
+const app = new Hono();
+
+app.get("/api/notify", (c) => {
+  return c.json({ message: "Notification service is running!" });
+});
+
+
+export default app;
