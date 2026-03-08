935a62591aa1fc09e3a5361145d32b2e3562c4a3
YourGitHubUsername <ramuk13476@gmail.com>
2026-03-08 22:11:46 +0530

    updated service

--- apps/notification-service/src/index.ts
+++ apps/notification-service/src/index.ts
@@ -1,11 +1,11 @@
 
 import Hono from "hono";
 
-//@ts-ignore
+
 const app = new Hono();
 
 app.get("/api/notify", (c) => {
-  return c.json({ message: "Notification service is running!" });
+  return c.json({ message: "Notification service is running on port !" });
 });
 
 
